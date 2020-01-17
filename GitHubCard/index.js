38bf4d2edd34329f1e18d4d1651413b080ec46ca
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-16 18:07:40 -0800

    project done except for making links clickable

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,16 +4,6 @@
 */
 
 
- axios.get("https://api.github.com/users/jkuenzinger")
-  .then(response => {
-   console.log(response);
-   response.data.forEach(item =>{
-     const newCard = (item);
-    })
-  })
-  .catch(error =>{
-    console.log('The data was not returned', error)
-  })
 
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
@@ -37,51 +27,67 @@
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
-
-
-
-function cardMaker(giturl) {
+function cardMaker (gitUrl) {
   const newCard = document.createElement('div'),
         newImage = document.createElement('img'),
-        newInformation = document.createLElemnt('div'),
-        newName= document.createElement('h3'),
-        userName= document.createElement('p'),
-        location= document.createElement('p'),
-        profile= document.createElement('p'),
+        newInformation = document.createElement('div'),
+        newName = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
         followers = document.createElement('p'),
         following = document.createElement('p'),
         bio = document.createElement('p')
-
-
-newCard.append(newImage);
-newCard.append(newInformation);
-newInformation.append(newName);
-newInformation.append(userName);
-newInformation.append(location);
-newInformation.append(profile);
-newInformation.append(followers);
-newInformation.append(following);
-newInformation.append(bio);
-
-newCard.classList.add('card');
-newInformation.classList.add('card-info');
-newName.classList.add('name');
-userName.classList.add('username');
-
-newImage.src = 'avatar_url';
-newName.textContent = 'name';
-userName.textContent = 'login';
-location.textContent = 'location';
-profile.textContent = 'html_url';
-followers.textContent = 'followers';
-following.textContent = 'following';
-bio.textContent = 'bio';
-
-return newCard;
+  newCard.append(newImage);
+  newCard.append(newInformation);
+  newInformation.append(newName);
+  newInformation.append(userName);
+  newInformation.append(location);
+  newInformation.append(profile);
+  newInformation.append(followers);
+  newInformation.append(following);
+  newInformation.append(bio);
+  newCard.classList.add('card');
+  newInformation.classList.add('card-info');
+  newName.classList.add('name');
+  userName.classList.add('username');
+  newImage.src = gitUrl.avatar_url
+  newName.textContent = `Name: ${gitUrl.name}`;
+  userName.textContent = `User Name: ${gitUrl.login}`;
+  location.textContent = `Location: ${gitUrl.location}`;
+  profile.textContent = `Profile: ${gitUrl.html_url}`;
+  followers.textContent =`Followers: ${gitUrl.followers}`;
+  following.textContent =`Following: ${gitUrl.following}`;
+  bio.textContent = gitUrl.bio;
+  return newCard;
 }
 const entryPoint = document.querySelector('.cards')
+
+axios.get('https://api.github.com/users/jKuenzinger')
+.then(response => {
+    entryPoint.append(cardMaker(response.data))
+})
+.catch(error => {
+  console.log('The data was not returned', error)
+})
+
+
+
+
+const followersArray = ['wildcard329', 'MarioR81', 'sdelpercio', 'cristinaedens', 'BrandyBecker' ];
+
+users = followersArray.map(user => 'https://api.github.com/users/' .concat(user))
+
+users.forEach(user => {
+  axios.get(user)
+  .then(response =>{
+    entryPoint.append(cardMaker(response.data))
+  })
+})
+
+
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
